0d3bf6597ec1507f80c9f04a75e7dd8e6d194846
navono <navono007@gmail.com>
2022-02-25 17:04:40 +0800

    feat: 精简 prototype 的界面布局

--- theia-extensions/ecsnext-prototype/src/browser/ecsnext-prototype-frontend-module.ts
+++ theia-extensions/ecsnext-prototype/src/browser/ecsnext-prototype-frontend-module.ts
@@ -1,11 +1,17 @@
 import { ContainerModule } from '@theia/core/shared/inversify';
+import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from '@theia/core/lib/browser';
+import { FileNavigatorContribution as TheiaFileNavigatorContribution } from '@theia/navigator/lib/browser/navigator-contribution';
+import { ScmContribution as TheiaScmContribution } from '@theia/scm/lib/browser/scm-contribution';
+
+import { FileNavigatorContribution } from './theia/navigator/navigator-contribution';
+import { ScmContribution } from './theia/scm/scm-contribution';
+
 import { EcsnextPrototypeWidget } from './ecsnext-prototype-widget';
 import { EcsnextPrototypeContribution } from './ecsnext-prototype-contribution';
-import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from '@theia/core/lib/browser';
 
 import '../../src/browser/style/index.css';
 
-export default new ContainerModule(bind => {
+export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bindViewContribution(bind, EcsnextPrototypeContribution);
     bind(FrontendApplicationContribution).toService(EcsnextPrototypeContribution);
     bind(EcsnextPrototypeWidget).toSelf();
@@ -13,4 +19,7 @@ export default new ContainerModule(bind => {
         id: EcsnextPrototypeWidget.ID,
         createWidget: () => ctx.container.get<EcsnextPrototypeWidget>(EcsnextPrototypeWidget)
     })).inSingletonScope();
+
+    rebind(TheiaFileNavigatorContribution).to(FileNavigatorContribution).inSingletonScope();
+    rebind(TheiaScmContribution).to(ScmContribution).inSingletonScope();
 });
---
+++ theia-extensions/ecsnext-prototype/src/browser/theia/navigator/navigator-tab-bar-decorator.ts
@@ -0,0 +1,19 @@
+import { injectable } from '@theia/core/shared/inversify';
+import { WidgetDecoration } from '@theia/core/lib/browser/widget-decoration';
+import { NavigatorTabBarDecorator as TheiaNavigatorTabBarDecorator } from '@theia/navigator/lib/browser/navigator-tab-bar-decorator';
+
+/**
+ * To silent the badge decoration in the `Explorer`.
+ * https://github.com/eclipse-theia/theia/issues/8709
+ */
+@injectable()
+export class NavigatorTabBarDecorator extends TheiaNavigatorTabBarDecorator {
+  onStart(): void {
+    // NOOP
+  }
+
+  decorate(): WidgetDecoration.Data[] {
+    // Does not decorate anything.
+    return [];
+  }
+}
